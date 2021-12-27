c5b373f20aed5ceb6e8d1f1c67670c4741c9c1d4
likaci <likacix@gmail.com>
2021-12-27 21:55:40 +0800

    remove App, add NetworkInfo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NetworkInfo from './NetworkInfo'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    (
+        <ul>
+            <NetworkInfo name={"Direct"} url={"http://direct.ipapi.xiazhiri.com/json/?lang=zh-CN"}/>
+            <NetworkInfo name={"Home"} url={"http://home.ipapi.xiazhiri.com/json/?lang=zh-CN"}/>
+            <NetworkInfo name={"Proxy"} url={"http://proxy.ipapi.xiazhiri.com/json/?lang=zh-CN"}/>
+            <NetworkInfo name={"Work"} url={"http://work.ipapi.xiazhiri.com/json/?lang=zh-CN"}/>
+        </ul>
+    ),
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
