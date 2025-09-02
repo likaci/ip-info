d7aa291ec751a67dc1d1ea6d7b8df5287f39cf32
likaci <3407980+likaci@users.noreply.github.com>
2025-09-01 23:21:36 -0400

    add cn

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default async function Page() {
   return <div className="grid gap-4 p-4 xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-2" >
     <NetworkInfo name="Direct" url={'https://direct.ipapi.xiazhiri.com/json'} />
     <NetworkInfo name="Proxy" url={'https://proxy.ipapi.xiazhiri.com/json'} />
+    <NetworkInfo name="CN" url={'https://cn.ipapi.xiazhiri.com/json'} />
     <NetworkInfo name="Home" url={'https://home.ipapi.xiazhiri.com/json'} />
     <NetworkInfo name="Work" url={'https://work.ipapi.xiazhiri.com/json'} />
   </div>
